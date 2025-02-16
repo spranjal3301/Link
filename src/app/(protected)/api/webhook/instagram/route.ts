09a778fd524e9a75ec8fe9fb6bbab1ef3dc66c8c
Pranjal <spranjal394@gmail.com>
2025-02-16 19:00:35 +0530

    -

--- src/app/(protected)/api/webhook/instagram/route.ts
+++ src/app/(protected)/api/webhook/instagram/route.ts
@@ -1,5 +1,9 @@
 import { matchUserKeyword } from "@/actions/instagram/queries";
-import { handleKeywordMatch, handleOngoingAiChat, handleReelReaction } from "@/lib/instagram-utils";
+import {
+  handleKeywordMatch,
+  handleOngoingAiChat,
+  handleReelReaction,
+} from "@/lib/instagram-utils";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
@@ -15,74 +19,66 @@ export async function POST(req: NextRequest) {
     const instagramId = entry?.id;
 
     const eventType = messaging
-    ? "message"
-    : changes?.[0]?.field === "comments"
-    ? "comment"
-    : null;
+      ? "message"
+      : changes?.[0]?.field === "comments"
+      ? "comment"
+      : null;
     const userText = messaging?.[0]?.message?.text ?? changes?.[0]?.value?.text;
-    console.log("userText",userText);
-    
+    console.log("userText", userText);
 
-    const isReel = messaging?.[0]?.message?.attachments?.[0]?.type === 'ig_reel';
-    console.log("isReel",isReel);
+    const isReel =
+      messaging?.[0]?.message?.attachments?.[0]?.type === "ig_reel";
+    console.log("isReel", isReel);
 
+    if (isReel) {
+      console.log("Reel Reaction");
 
-    if(isReel){
-        console.log("Reel Reaction");
-        
-        const MessageId = messaging?.[0]?.message?.mid;
-        return createResponse("Reel Reaction ❤️",200);
-        handleReelReaction(instagramId,messaging[0].sender.id,MessageId);
+      const MessageId = messaging?.[0]?.message?.mid;
+      return createResponse("Reel Reaction ❤️", 200);
+      handleReelReaction(instagramId, messaging[0].sender.id, MessageId);
     }
 
-    if(!userText || !instagramId)return createResponse("userText undefind",200);
+    if (!userText || !instagramId)
+      return createResponse("userText undefind", 200);
 
-    console.log("webhookPayload",webhookPayload);
-    console.log("instagramId",entry);
+    console.log("webhookPayload", webhookPayload);
+    console.log("instagramId", entry);
     console.log(messaging?.[0]?.message);
-    console.log("eventType",eventType);
-    console.log("userText",userText);
+    console.log("eventType", eventType);
+    console.log("userText", userText);
 
-    const matcher = await matchUserKeyword(instagramId,userText);
+    const matcher = await matchUserKeyword(instagramId, userText);
 
-
-
-    
-    if(matcher && matcher?.automationId && eventType){
-      const isSelfReply =  eventType == 'message' ? 
-                          (entry.id==messaging?.[0]?.sender?.id) :
-                          (entry.id==changes?.[0]?.value?.id);
+    if (matcher && matcher?.automationId && eventType) {
+      const isSelfReply =
+        eventType == "message"
+          ? entry.id == messaging?.[0]?.sender?.id
+          : entry.id == changes?.[0]?.value?.id;
 
       console.log(isSelfReply);
-                          
-      if(isSelfReply)return createResponse("Self Reply Event",200);
 
-      handleKeywordMatch(
+      if (isSelfReply) return createResponse("Self Reply Event", 200);
+
+      await handleKeywordMatch(
         matcher.automationId,
         eventType,
         userText,
         messaging,
         changes,
         entry
-      )
+      );
     }
 
-
-
-
-    if(!matcher && eventType === "message"){
-      handleOngoingAiChat(messaging,entry);
+    if (!matcher && eventType === "message") {
+      await handleOngoingAiChat(messaging, entry);
     }
 
-
-    return createResponse("Success",200);
-
+    return createResponse("Success", 200);
   } catch (error) {
-     console.log("Error while webhook⚠️❌🚨⛔", error);
-    return createResponse("Error while webhook",200);
-  }  
+    console.log("Error while webhook⚠️❌🚨⛔", error);
+    return createResponse("Error while webhook", 200);
+  }
 }
 
-
 const createResponse = (message: string, status: number) =>
-    NextResponse.json({ message }, { status: 200 });
+  NextResponse.json({ message }, { status: 200 });
